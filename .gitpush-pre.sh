4a98b4c1ef2b9e523a527fb2b09dac2caa32450f
Elazar Pimentel <elazar.pimentel@gmail.com>
2026-03-01 22:12:20 -0300

    2026-03-01-2212

--- .gitpush-pre.sh
+++ .gitpush-pre.sh
@@ -1,1 +1,5 @@
-# no checks needed
+# Sync version from index.html to en/ and he/ (sitebump only bumps root)
+VER=$(grep -oP '<span class="version">v\K[0-9]+\.[0-9]+\.[0-9]+' index.html)
+if [[ -n "$VER" ]]; then
+    sed -i "s|<span class=\"version\">v[0-9]*\.[0-9]*\.[0-9]*</span>|<span class=\"version\">v${VER}</span>|" en/index.html he/index.html
+fi
