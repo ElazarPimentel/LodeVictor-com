560bdc2b0d528fd27d422c926cb7e86ae88a8a2d
Elazar Pimentel <elazar.pimentel@gmail.com>
2026-09-19 02:21:33 -0300

    2026-09-19-0221 remove version-sync hook; sync en/he by hand
    Agent: webmaster-lodevictorcom-cc

--- .gitpush-pre.sh
+++ .gitpush-pre.sh
@@ -1,5 +1,1 @@
-# Sync version from index.html to en/ and he/ (sitebump only bumps root)
-VER=$(grep -oP '<span class="version">v\K[0-9]+\.[0-9]+\.[0-9]+' index.html)
-if [[ -n "$VER" ]]; then
-    sed -i "s|<span class=\"version\">v[0-9]*\.[0-9]*\.[0-9]*</span>|<span class=\"version\">v${VER}</span>|" en/index.html he/index.html
-fi
+# GITPUSH_INFO_VERSION=4.62
